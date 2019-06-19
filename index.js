e528765c3da590baac1220fc87b87ad61f91b9f9
peter sigurdson <peter@petersigurdson.net>
2019-06-19 14:51:54 -0400

    commit 3

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-// basic way to create an OBJECT:
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
+// factory function
+function createCircle(radius){
+ return {
+    radius,
     draw: function(){
         console.log('draw');
     }
-};
+};   
+}
+const circle = createCircle(1);
+circle.draw();
 
+// Constructor Function
+
+function Circle(radius){
+    console.log('this', this);
+    this.radius = radius;
+    this.draw = function(){
+        console.log("draw");
+    }
+}
+
+const another = new Circle(1);
